411e0ab2faeb555252192b3165e030f9a1847a0b
Taron <88250538+taronavagyan@users.noreply.github.com>
2023-11-05 08:59:48 -0800

    refactor(src): :recycle: edit app to use usersRouter
    import usersRouter into index.js and have it handle all /users routes
    
    remove unused /users routes now that they have been migrated to usersRouter

--- src/index.js
+++ src/index.js
@@ -6,11 +6,7 @@ import cors from 'cors';
 import './dataAccess/loadEnv.js';
 
 import secretsRouter from './routes/secrets.js';
-import getAllUsers from './dataAccess/getAllUsers.js';
-import addUser from './dataAccess/addUser.js';
-import getUserPermissions from './dataAccess/getUserPermissions.js';
-import editUserPermission from './dataAccess/editUserPermission.js';
-import deleteUser from './dataAccess/deleteUser.js';
+import usersRouter from './routes/users.js';
 import client from './dataAccess/dbClient.js';
 import Secrets from './dataAccess/model.js';
 import syncTable from './dataAccess/syncTable.js';
@@ -74,87 +70,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/secrets', secretsRouter);
-// Delete a secret by  key
-app.delete('/users/:username', async (req, res) => {
-    try {
-        const { username } = req.params;
-        /* 
-    deleteUser returns 1 if a user was found and deleted, 0 otherwise
-    */
-        const userDeleted = !!(await deleteUser(res.locals.dbClient, username));
-
-        if (!userDeleted) {
-            res.status(404).json({ message: 'User could not be deleted or was not found.' });
-        } else {
-            res.status(204).send();
-        }
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-});
-
-// Get all users
-app.get('/users', async (req, res) => {
-    const users = await getAllUsers(res.locals.dbClient);
-    res.status(200).json(users);
-});
-
-// Create a user
-app.post('/users', async (req, res) => {
-    try {
-        const usersCreated = await addUser(
-            res.locals.dbClient,
-            req.body.username,
-            req.body.hasWritePermissions
-        );
-        res.status(201).json({ usersCreated });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-});
-
-// Get a user's permissions by username
-app.get('/users/:username', async (req, res) => {
-    try {
-        const { username } = req.params;
-
-        const userPermissions = await getUserPermissions(res.locals.dbClient, username);
-
-        if (userPermissions.length === 0) {
-            res.status(404).json({
-                message: 'User was not found',
-            });
-        } else {
-            res.status(200).json(userPermissions);
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-});
-
-// Edit a user's read/write permission
-app.post('/users/:username', async (req, res) => {
-    try {
-        const editPermissionResult = await editUserPermission(
-            res.locals.dbClient,
-            req.body.username,
-            req.body.setWritePermissionTo
-        );
-
-        if (editPermissionResult === undefined) {
-            res.status(404).json({
-                message: 'User was not found',
-            });
-        } else {
-            res.status(200).json(editPermissionResult);
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-});
-
-// app.listen(process.env.API_PORT, () => {
-//     console.log(`Embrasure server running on port ${process.env.API_PORT}`);
-// });
+app.use('/users', usersRouter);
 
 export const handler = serverless(app);
