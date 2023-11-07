c7a0b86c1a61b5fe17c92f5d84e44a5ad658556a
ivanfslee <57885956+ivanfslee@users.noreply.github.com>
2023-11-06 20:53:17 -0800

    refactor(addLog.js):  :recycle: remove console.log in addLog.js
    remove console.log in addLog.js

--- src/dataAccess/addLog.js
+++ src/dataAccess/addLog.js
@@ -1,6 +1,5 @@
 async function addLog(table, logData) {
     try {
-        console.log('Creating log record');
         await table.create(logData);
     } catch (error) {
         console.error('Error in creating a new log: ', error);
