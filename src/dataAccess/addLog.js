438ba80d3f6201bbb6f170aca9bdb9204880dc3b
ivanfslee <57885956+ivanfslee@users.noreply.github.com>
2023-11-10 14:56:38 -0800

    fix(addLog.js): :alembic: add transaction commit and rollback to addLog.js
    add transaction commit and rollback to addLog.js
    
    Co-Authored-By: Chase Sobey <116530802+ChaseSobey@users.noreply.github.com>
    Co-Authored-By: Taron <88250538+taronavagyan@users.noreply.github.com>

--- src/dataAccess/addLog.js
+++ src/dataAccess/addLog.js
@@ -1,8 +1,14 @@
-async function addLog(table, logData) {
+async function addLog(table, logData, client) {
+    const transaction = await client.transaction();
+
     try {
-        await table.create(logData);
+        console.log('in addLog - about to add log');
+        await table.create(logData, { transaction });
+        await transaction.commit();
+        console.log('in addLog - finished adding log');
     } catch (error) {
         console.error('Error in creating a new log: ', error);
+        await transaction.rollback();
         throw error;
     }
 }
